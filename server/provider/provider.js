556338613116476e554260b681e9cdb9cf6a7077
amanaj10896 <aman.javed@Triconinfotech.com>
2018-11-02 19:05:43 +0530

    provider file added

--- server/provider/provider.js
+++ server/provider/provider.js
@@ -1,7 +1,27 @@
-aggregator = (userRequest , aggregator)=>{
-    var a = ["aaaa","ggg"];
-    return {aggregator:a};
+const axios = require('axios');
+var providerHandler={};
+var detail;
+
+providerHandler.goibibo = async function (userRequest,busAggregator) {
+    url = `${busAggregator.url}app_id=${busAggregator.appId}&app_key=${busAggregator.appKey}&format=json&source=${userRequest.source}&destination=${userRequest.destination}&dateofdeparture=${userRequest.ddate}`;
+    console.log(url);
+    await axios.get(url).then((response)=>{
+        detail = response.data.data.onwardflights;
+        
+});
+return(detail);
 }
+
+aggregator = async (userRequest , busAggName , busAggregator)=>{
+    
+await providerHandler[busAggName](userRequest,busAggregator).then((response)=>{
+    detail = response;
+});
+// console.log(detail);
+// console.log(detail);
+return detail;
+}
+
 
 module.exports={
     aggregator
